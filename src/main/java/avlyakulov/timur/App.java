95fa9aa70503eff81d59f3bdf9d34673aefb009b
timcol1 <avlyakulovtimur@gmail.com>
2024-01-10 10:35:18 +0200

    finish example for sqlite with people

--- src/main/java/avlyakulov/timur/App.java
+++ src/main/java/avlyakulov/timur/App.java
@@ -1,13 +1,8 @@
 package avlyakulov.timur;
 
-import org.w3c.dom.ls.LSOutput;
-
 import java.sql.*;
 import java.util.Scanner;
 
-/**
- * Hello world!
- */
 public class App {
 
 
@@ -15,23 +10,20 @@ public class App {
         App app = new App();
 
         app.open();
-        app.insert();
-        app.close();
     }
 
-    Connection connection;
 
     void open() {
-        try {
-            Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection("jdbc:sqlite:sqlite/currencies.db");
+        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:sqlite/users.db")) {
             System.out.println("Connected");
+            select(connection);
         } catch (Exception e) {
             System.out.println(e.getMessage());
+
         }
     }
 
-    void insert() {
+    void insert(Connection connection) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter user's name:");
         String name = scanner.nextLine();
@@ -50,13 +42,64 @@ public class App {
         System.out.println("Rows added");
     }
 
-
-    void close() {
+    void select(Connection connection) {
+        String query = "SELECT * from users;";
         try {
-            connection.close();
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                User user = new User(resultSet.getInt("id"), resultSet.getString("name"), resultSet.getString("phone"));
+                System.out.println(user);
+            }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+        System.out.println("Rows were taken");
+    }
+}
 
+class User {
+    private int id;
+    private String name;
+
+    private String phone;
+
+    public User(int id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
     }
 }
