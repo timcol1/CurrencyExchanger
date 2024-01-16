64265bf93d60cbacc0e773425f1940ce4a177f9d
timcol1 <avlyakulovtimur@gmail.com>
2024-01-16 11:42:04 +0200

    add example servlet to check time

--- src/main/java/avlyakulov/timur/controller/TestServlet.java
+++ src/main/java/avlyakulov/timur/controller/TestServlet.java
@@ -5,15 +5,30 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 //аналог web.xml тоже самое но лучше
 @WebServlet(name = "Test", urlPatterns = {"/test"})
 public class TestServlet extends HttpServlet {
+
+    private static Logger logger = LoggerFactory.getLogger(TestServlet.class);
+
+
+    //здесь мы можем уже задавать какие то классы и прочее, то есть инициализация классов в сервлете
+    //срабатывает 1 раз и все
+    @Override
+    public void init() throws ServletException {
+        logger.info("Servlet is created");
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("It is answer from servlet");
+        req.setCharacterEncoding("UTF-8");
+        String surname = req.getParameter("surname");
+        resp.getWriter().println(surname);
     }
 
     @Override
---
+++ src/main/webapp/time.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Timur
+  Date: 16.01.2024
+  Time: 11:33
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Time</title>
+</head>
+<body>
+<h1>Current server time: <%=request.getAttribute("date")%></h1>
+</body>
+</html>
