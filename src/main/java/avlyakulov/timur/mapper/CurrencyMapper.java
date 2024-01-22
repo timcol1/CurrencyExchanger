c7f32d89ee00fc5fd326dc81a0f56a8cfb163b10
timcol1 <avlyakulovtimur@gmail.com>
2024-01-22 12:34:50 +0200

    create implementation currency mapper

---
+++ src/main/java/avlyakulov/timur/mapper/CurrencyMapper.java
@@ -0,0 +1,27 @@
+package avlyakulov.timur.mapper;
+
+import avlyakulov.timur.dto.currency.CurrencyRequest;
+import avlyakulov.timur.dto.currency.CurrencyResponse;
+import avlyakulov.timur.model.Currency;
+
+public class CurrencyMapper implements RequestMapper<CurrencyRequest, Currency>, ResponseMapper<Currency, CurrencyResponse> {
+
+    @Override
+    public Currency mapToEntity(CurrencyRequest currencyRequest) {
+        return new Currency(
+                currencyRequest.getCode(),
+                currencyRequest.getFullName(),
+                currencyRequest.getSign()
+        );
+    }
+
+    @Override
+    public CurrencyResponse mapToResponse(Currency currency) {
+        return new CurrencyResponse(
+                currency.getId(),
+                currency.getCode(),
+                currency.getFullName(),
+                currency.getSign()
+        );
+    }
+}
