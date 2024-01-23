0115a14cd7ba2073d26357e71db992dc59390edd
timcol1 <avlyakulovtimur@gmail.com>
2024-01-23 13:38:54 +0200

    add methods to dao for get exchange rate by code

--- src/main/java/avlyakulov/timur/dao/ExchangeRateDao.java
+++ src/main/java/avlyakulov/timur/dao/ExchangeRateDao.java
@@ -3,9 +3,11 @@ package avlyakulov.timur.dao;
 import avlyakulov.timur.connection.ConnectionBuilder;
 import avlyakulov.timur.model.ExchangeRate;
 
+import java.util.Optional;
+
 public interface ExchangeRateDao extends CrudDao<ExchangeRate, Integer> {
 
-    ExchangeRate findByCodes(String baseCurrencyCode, String targetCurrencyCode);
+    Optional<ExchangeRate> findByCodes(String baseCurrencyCode, String targetCurrencyCode);
 
     void setConnectionBuilder(ConnectionBuilder connectionBuilder);
 }
--- src/main/java/avlyakulov/timur/dao/ExchangeRateDaoImpl.java
+++ src/main/java/avlyakulov/timur/dao/ExchangeRateDaoImpl.java
@@ -11,13 +11,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 public class ExchangeRateDaoImpl implements ExchangeRateDao {
 
     private ConnectionBuilder connectionBuilder;
 
-    @Override
     public void setConnectionBuilder(ConnectionBuilder connectionBuilder) {
         this.connectionBuilder = connectionBuilder;
     }
@@ -30,46 +30,46 @@ public class ExchangeRateDaoImpl implements ExchangeRateDao {
     @Override
     public List<ExchangeRate> findAll() {
         final String findAllQuery = "SELECT er.ID,\n" +
-                "       bc.ID,\n" +
-                "       bc.Code,\n" +
-                "       bc.FullName,\n" +
-                "       bc.Sign,\n" +
-                "       tc.ID,\n" +
-                "       tc.Code,\n" +
-                "       tc.FullName,\n" +
-                "       tc.Sign,\n" +
+                "       bc.ID AS BaseCurrencyId,\n" +
+                "       bc.Code AS BaseCurrencyCode,\n" +
+                "       bc.FullName AS BaseCurrencyFullName,\n" +
+                "       bc.Sign AS BaseCurrencySign,\n" +
+                "       tc.ID AS TargetCurrencyId,\n" +
+                "       tc.Code AS TargetCurrencyCode,\n" +
+                "       tc.FullName AS TargetCurrencyFullName,\n" +
+                "       tc.Sign AS TargetCurrencySign,\n" +
                 "       Rate\n" +
                 "FROM ExchangeRates er\n" +
                 "         INNER JOIN Currencies AS bc ON er.BaseCurrencyId = bc.ID\n" +
                 "         INNER JOIN Currencies AS tc ON er.TargetCurrencyId = tc.ID;\n";
 
-        List<ExchangeRate> exchangeRates = new ArrayList<>();
         try (Connection connection = getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(findAllQuery)) {
             Currency baseCurrency;
             Currency targetCurrency;
             ResultSet resultSet = preparedStatement.executeQuery();
+            List<ExchangeRate> exchangeRates = new ArrayList<>();
             while (resultSet.next()) {
                 baseCurrency = new Currency(
-                        resultSet.getInt(2),
-                        resultSet.getString(3),
-                        resultSet.getString(4),
-                        resultSet.getString(5)
+                        resultSet.getInt("BaseCurrencyId"),
+                        resultSet.getString("BaseCurrencyCode"),
+                        resultSet.getString("BaseCurrencyFullName"),
+                        resultSet.getString("BaseCurrencySign")
                 );
 
                 targetCurrency = new Currency(
-                        resultSet.getInt(6),
-                        resultSet.getString(7),
-                        resultSet.getString(8),
-                        resultSet.getString(9)
+                        resultSet.getInt("TargetCurrencyId"),
+                        resultSet.getString("TargetCurrencyCode"),
+                        resultSet.getString("TargetCurrencyFullName"),
+                        resultSet.getString("TargetCurrencySign")
                 );
-                ExchangeRate currency = new ExchangeRate(
-                        resultSet.getInt(1),
+                ExchangeRate exchangeRate = new ExchangeRate(
+                        resultSet.getInt("ID"),
                         baseCurrency,
                         targetCurrency,
-                        resultSet.getBigDecimal(10)
+                        resultSet.getBigDecimal("Rate")
                 );
-                exchangeRates.add(currency);
+                exchangeRates.add(exchangeRate);
             }
             return exchangeRates;
         } catch (SQLException e) {
@@ -79,12 +79,59 @@ public class ExchangeRateDaoImpl implements ExchangeRateDao {
     }
 
     @Override
-    public ExchangeRate create(ExchangeRate exchangeRate) {
-        return null;
+    public Optional<ExchangeRate> findByCodes(String baseCurrencyCode, String targetCurrencyCode) {
+        final String findExchangeRateQuery = "SELECT er.ID,\n" +
+                "       bc.ID       AS BaseCurrencyId,\n" +
+                "       bc.Code     AS BaseCurrencyCode,\n" +
+                "       bc.FullName AS BaseCurrencyFullName,\n" +
+                "       bc.Sign     AS BaseCurrencySign,\n" +
+                "       tc.ID       AS TargetCurrencyId,\n" +
+                "       tc.Code     AS TargetCurrencyCode,\n" +
+                "       tc.FullName AS TargetCurrencyFullName,\n" +
+                "       tc.Sign     AS TargetCurrencySign,\n" +
+                "       Rate\n" +
+                "FROM ExchangeRates er\n" +
+                "         INNER JOIN Currencies AS bc ON er.BaseCurrencyId = bc.ID\n" +
+                "         INNER JOIN Currencies AS tc ON er.TargetCurrencyId = tc.ID\n" +
+                "WHERE BaseCurrencyCode = ? AND TargetCurrencyCode = ?;";
+
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(findExchangeRateQuery)) {
+            preparedStatement.setString(1, baseCurrencyCode);
+            preparedStatement.setString(2, targetCurrencyCode);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                Currency baseCurrency = new Currency(
+                        resultSet.getInt("BaseCurrencyId"),
+                        resultSet.getString("BaseCurrencyCode"),
+                        resultSet.getString("BaseCurrencyFullName"),
+                        resultSet.getString("BaseCurrencySign")
+                );
+
+                Currency targetCurrency = new Currency(
+                        resultSet.getInt("TargetCurrencyId"),
+                        resultSet.getString("TargetCurrencyCode"),
+                        resultSet.getString("TargetCurrencyFullName"),
+                        resultSet.getString("TargetCurrencySign")
+                );
+                ExchangeRate exchangeRate = new ExchangeRate(
+                        resultSet.getInt("ID"),
+                        baseCurrency,
+                        targetCurrency,
+                        resultSet.getBigDecimal("Rate")
+                );
+                return Optional.of(exchangeRate);
+            }
+        } catch (SQLException e) {
+            log.error("Error with db");
+            throw new RuntimeException(e);
+        }
+        return Optional.empty();
     }
 
     @Override
-    public ExchangeRate findByCodes(String baseCurrencyCode, String targetCurrencyCode) {
+    public ExchangeRate create(ExchangeRate exchangeRate) {
         return null;
     }
 }
