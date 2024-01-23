bdabe90b8bf098d9b19fc3c093d297cc2cb8637a
timcol1 <avlyakulovtimur@gmail.com>
2024-01-23 13:37:16 +0200

    create exceptions for handling ExchangeRateServlet

---
+++ src/main/java/avlyakulov/timur/custom_exception/ExchangeRateCurrencyCodePairException.java
@@ -0,0 +1,11 @@
+package avlyakulov.timur.custom_exception;
+
+public class ExchangeRateCurrencyCodePairException extends RuntimeException {
+    public ExchangeRateCurrencyCodePairException() {
+
+    }
+
+    public ExchangeRateCurrencyCodePairException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/avlyakulov/timur/custom_exception/ExchangeRateCurrencyPairNotFoundException.java
@@ -0,0 +1,10 @@
+package avlyakulov.timur.custom_exception;
+
+public class ExchangeRateCurrencyPairNotFoundException extends RuntimeException {
+    public ExchangeRateCurrencyPairNotFoundException() {
+    }
+
+    public ExchangeRateCurrencyPairNotFoundException(String message) {
+        super(message);
+    }
+}
