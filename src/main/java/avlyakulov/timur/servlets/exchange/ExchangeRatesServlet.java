9dd8742a841b28fff4097f522b10125be8c512dd
timcol1 <avlyakulovtimur@gmail.com>
2024-01-23 12:54:58 +0200

    change name servlet from ExchangeRateServlet to ExchangeRatesServlet

---
+++ src/main/java/avlyakulov/timur/servlets/exchange/ExchangeRatesServlet.java
@@ -0,0 +1,40 @@
+package avlyakulov.timur.servlets.exchange;
+
+import avlyakulov.timur.dto.exchange.ExchangeRateResponse;
+import avlyakulov.timur.mapper.ExchangeRateMapper;
+import avlyakulov.timur.service.ExchangeRateService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@Slf4j
+@WebServlet(urlPatterns = "/exchangeRates")
+public class ExchangeRatesServlet extends HttpServlet {
+
+    private final ExchangeRateService exchangeRateService = new ExchangeRateService();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    ExchangeRateMapper exchangeRateMapper = new ExchangeRateMapper();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.info("We are getting all exchange rates");
+        List<ExchangeRateResponse> exchangeRateResponses = exchangeRateService.findAll().stream().map(exchangeRateMapper::mapToResponse).toList();
+        PrintWriter out = resp.getWriter();
+        resp.setStatus(HttpServletResponse.SC_OK);//status 200
+        out.print(objectMapper.writeValueAsString(exchangeRateResponses));
+        out.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
