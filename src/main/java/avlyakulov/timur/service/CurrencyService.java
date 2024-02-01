2868ca74c69e25cca28c6b2fc6ae3b9bd597c862
Timur <91984444+timcol1@users.noreply.github.com>
2024-02-01 17:40:31 +0200

    add abstraction between service

--- src/main/java/avlyakulov/timur/service/CurrencyService.java
+++ src/main/java/avlyakulov/timur/service/CurrencyService.java
@@ -1,52 +1,14 @@
 package avlyakulov.timur.service;
 
-import avlyakulov.timur.connection.ConnectionBuilder;
-import avlyakulov.timur.custom_exception.BadCurrencyCodeException;
-import avlyakulov.timur.custom_exception.CurrencyAlreadyExistsException;
-import avlyakulov.timur.custom_exception.CurrencyNotFoundException;
-import avlyakulov.timur.dao.CurrencyDao;
-import avlyakulov.timur.dao.CurrencyDaoImpl;
 import avlyakulov.timur.model.Currency;
 
 import java.util.List;
-import java.util.Optional;
 
-public class CurrencyService {
-    private final int CODE_LENGTH_URL = 3;
+public interface CurrencyService {
 
-    CurrencyDao currencyDao = new CurrencyDaoImpl();
+    List<Currency> findAll();
 
-    public void setConnectionBuilder(ConnectionBuilder connectionBuilder) {
-        currencyDao.setConnectionBuilder(connectionBuilder);
-    }
+    Currency findByCode(String code);
 
-    public List<Currency> findAll() {
-        return currencyDao.findAll();
-    }
-
-    public Currency findByCode(String code) {
-        if (code.length() != CODE_LENGTH_URL) {
-            throw new BadCurrencyCodeException("Currency code is missing at address or you put wrong code");
-        } else {
-            Optional<Currency> currency = currencyDao.findCurrencyByCode(code);
-            if (currency.isEmpty()) {
-                throw new CurrencyNotFoundException("Currency with this code " + code + " wasn't found");
-            } else {
-                return currency.get();
-            }
-        }
-    }
-
-    public Currency createCurrency(Currency currency) {
-        Optional<Currency> currencyOptional = findSimpleByCode(currency.getCode());
-        if (currencyOptional.isPresent()) {
-            throw new CurrencyAlreadyExistsException("Currency with such code " + currency.getCode() + " is already exists");
-        } else {
-            return currencyDao.create(currency);
-        }
-    }
-
-    private Optional<Currency> findSimpleByCode(String code) {
-        return currencyDao.findCurrencyByCode(code);
-    }
+    Currency createCurrency(Currency currency);
 }
---
+++ src/main/java/avlyakulov/timur/service/CurrencyServiceImpl.java
@@ -0,0 +1,52 @@
+package avlyakulov.timur.service;
+
+import avlyakulov.timur.connection.ConnectionBuilder;
+import avlyakulov.timur.custom_exception.BadCurrencyCodeException;
+import avlyakulov.timur.custom_exception.CurrencyAlreadyExistsException;
+import avlyakulov.timur.custom_exception.CurrencyNotFoundException;
+import avlyakulov.timur.dao.CurrencyDao;
+import avlyakulov.timur.model.Currency;
+
+import java.util.List;
+import java.util.Optional;
+
+public class CurrencyServiceImpl implements CurrencyService {
+    private final int CODE_LENGTH_URL = 3;
+
+    CurrencyDao currencyDao;
+
+    public CurrencyServiceImpl(CurrencyDao currencyDao) {
+        this.currencyDao = currencyDao;
+    }
+
+
+    public List<Currency> findAll() {
+        return currencyDao.findAll();
+    }
+
+    public Currency findByCode(String code) {
+        if (code.length() != CODE_LENGTH_URL) {
+            throw new BadCurrencyCodeException("Currency code is missing at address or you put wrong code");
+        } else {
+            Optional<Currency> currency = currencyDao.findCurrencyByCode(code);
+            if (currency.isEmpty()) {
+                throw new CurrencyNotFoundException("Currency with this code " + code + " wasn't found");
+            } else {
+                return currency.get();
+            }
+        }
+    }
+
+    public Currency createCurrency(Currency currency) {
+        Optional<Currency> currencyOptional = findSimpleByCode(currency.getCode());
+        if (currencyOptional.isPresent()) {
+            throw new CurrencyAlreadyExistsException("Currency with such code " + currency.getCode() + " is already exists");
+        } else {
+            return currencyDao.create(currency);
+        }
+    }
+
+    private Optional<Currency> findSimpleByCode(String code) {
+        return currencyDao.findCurrencyByCode(code);
+    }
+}
