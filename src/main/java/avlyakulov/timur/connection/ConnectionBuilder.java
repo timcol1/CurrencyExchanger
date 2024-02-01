23fbb56000b1f23afe8da8cce965faf7b0189f7b
Timur <avlyakulovtimur@gmail.com>
2024-02-01 18:43:52 +0200

    remove connection builder and context.xml

--- src/main/java/avlyakulov/timur/connection/ConnectionBuilder.java
+++
@@ -1,8 +0,0 @@
-package avlyakulov.timur.connection;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionBuilder {
-    Connection getConnection() throws SQLException;
-}
