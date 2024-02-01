810bf0150529f464ef07e5ef1ceb4f0c16716408
Timur <91984444+timcol1@users.noreply.github.com>
2024-02-01 17:40:57 +0200

    add hikari pool

---
+++ src/main/java/avlyakulov/timur/connection/DataSource.java
@@ -0,0 +1,32 @@
+package avlyakulov.timur.connection;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DataSource {
+    private static HikariConfig config = new HikariConfig();
+    private static HikariDataSource ds;
+
+    static {
+        config.setDriverClassName("org.sqlite.JDBC");
+        // Получение URL ресурса, связанного с классом
+        String classResourcePath = DataSource.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        // Выделение пути к каталогу приложения из пути к классу
+        String appPath = classResourcePath.substring(0, classResourcePath.indexOf("/target"));
+        config.setJdbcUrl("jdbc:sqlite:" + appPath + "/sqlite/currencies.db");
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        ds = new HikariDataSource(config);
+    }
+
+    private DataSource() {
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
+}
