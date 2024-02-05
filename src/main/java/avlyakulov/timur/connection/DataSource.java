5575d33ee624a962154a8558b5816bc4302e03d6
Timur <91984444+timcol1@users.noreply.github.com>
2024-02-05 13:02:05 +0200

    change from strict connection to relative

--- src/main/java/avlyakulov/timur/connection/DataSource.java
+++ src/main/java/avlyakulov/timur/connection/DataSource.java
@@ -2,21 +2,37 @@ package avlyakulov.timur.connection;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
+@Slf4j
 public class DataSource {
     private static final HikariConfig config = new HikariConfig();
     private static final HikariDataSource ds;
 
     static {
         config.setDriverClassName("org.sqlite.JDBC");
-        // Получение URL ресурса, связанного с классом
-        String classResourcePath = DataSource.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-        // Выделение пути к каталогу приложения из пути к классу
-        String appPath = classResourcePath.substring(0, classResourcePath.indexOf("/target"));
-        config.setJdbcUrl("jdbc:sqlite:" + appPath + "/sqlite/currencies.db");
+
+        URL resource = DataSource.class.getClassLoader().getResource("currencies.db");
+        String path = null;
+        if (resource != null) {
+            try {
+                path = new File(resource.toURI()).getAbsolutePath();
+            } catch (URISyntaxException e) {
+                log.error("Failed to connect to db");
+                throw new RuntimeException(e);
+            }
+        }
+        config.setJdbcUrl(String.format("jdbc:sqlite:%s", path));
         config.addDataSourceProperty("cachePrepStmts", "true");
         config.addDataSourceProperty("prepStmtCacheSize", "250");
         config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
